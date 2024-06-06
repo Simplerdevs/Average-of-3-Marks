1cc4c4fecf8a7cfd9e91937924ad62a00879db01
Simplerdevs <tplg7839@gmail.com>
2024-06-06 09:33:35 -0700

    Add project files.

---
+++ Average of 3 Marks/Average of 3 Marks.cpp
@@ -0,0 +1,32 @@
+
+                    /* Program to ask the user enter three marks then calculate the average of the Entered marks */
+
+#include <iostream>
+using namespace std;
+                    
+
+int main()
+
+{
+
+    int mark1, mark2, mark3;
+    cout << "Please Enter the first Mark ! " << endl;
+    cin >> mark1;
+
+    cout << "Please Enter the Second Mark ! " << endl;
+    cin >> mark2;
+
+    cout << "Please Enter the third Mark ! " << endl;
+    cin >> mark3;
+
+
+    cout << "The Average is : " << (mark1 + mark2 + mark3) / 3 << endl;
+
+
+
+
+    return 0;
+
+    // created by @ilyes_trabelsi
+
+}
